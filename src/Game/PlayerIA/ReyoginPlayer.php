3305f2050a435d8e9a2f0fea6e8ec455cb9e0541
Bertrand Ha <bertrand.ha@epita.fr>
2019-09-25 20:33:11 +0200

    Starting a little bit of intelligence

--- src/Game/PlayerIA/ReyoginPlayer.php
+++ src/Game/PlayerIA/ReyoginPlayer.php
@@ -36,11 +36,74 @@ class ReyoginPlayer extends Player
         // -------------------------------------    -----------------------------------------------------
         // How can i display the result of each round ? $this->prettyDisplay()
         // -------------------------------------    -----------------------------------------------------
+
+        // Lose = 0 ; Draw = 1; Win = 3
+
+        $this->prettyDisplay();
+
+        $mylastmove = $this->result->getLastChoiceFor($this->mySide);
+        $enemylastmove = $this->result->getLastChoiceFor($this->opponentSide);
+
+        $mylastresult = $this->result->getLastScoreFor($this->mySide);
+        $enemylastresult = $this->result->getLastScoreFor($this->opponentSide);
+
+        if ($mylastmove == '0')
+            return parent::scissorsChoice();
+
+        // Cases if I won
+        if ($mylastresult == '5'){
+            // Case if opponent lost ==> Play what he did last
+            if ($enemylastresult == '0')
+                return $enemylastmove;
+
+            // opponent drew ==> random
+            if ($enemylastresult == '1'){
+                $value = rand(1,3);
+                if ($value == 1)
+                    return parent::paperChoice();
+                elseif ($value == 2)
+                    return parent::rockChoice();
+                return parent::scissorsChoice();
+            }
+
+            // Opponent won ==> play what beat his last
+            if ($enemylastmove == 'paper')
+                return parent::scissorsChoice();
+            if ($enemylastmove == 'scissors')
+                return parent::rockChoice();
+            return parent::paperChoice();
+        }
+        // Cases if I lost
+        elseif ($mylastresult == '0'){
+            //Opponent won ==> play what beat his last
+            if ($enemylastresult == '5') {
+                if ($enemylastmove == 'paper')
+                    return parent::scissorsChoice();
+                if ($enemylastmove == 'scissors')
+                    return parent::rockChoice();
+                return parent::paperChoice();
+            }
+
+            //Opponent drew ==> random
+            if ($enemylastresult == '1'){
+                $value = rand(1,3);
+                if ($value == 1)
+                    return parent::paperChoice();
+                elseif ($value == 2)
+                    return parent::rockChoice();
+                return parent::scissorsChoice();
+            }
+
+            //Opponent lost ==> play his last
+            return $enemylastmove;
+        }
+
         $value = rand(1,3);
         if ($value == 1)
             return parent::paperChoice();
         elseif ($value == 2)
             return parent::rockChoice();
         return parent::scissorsChoice();
+
     }
 }
